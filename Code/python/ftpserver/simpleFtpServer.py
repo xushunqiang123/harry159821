e8d9cc1b018e0b901d2c28f0c12c78263f2c7dc0
harry159821 <harry159821@gmail.com>
2018-06-12 09:49:52 +0800

    Update foreign_addresses Permission
    Signed-off-by: harry159821 <harry159821@gmail.com>

--- Code/python/ftpserver/simpleFtpServer.py
+++ Code/python/ftpserver/simpleFtpServer.py
@@ -5,7 +5,7 @@ import os
 import socket
 
 def main():
-    # ip = socket.gethostbyname(socket.gethostname())
+    ip = socket.gethostbyname(socket.gethostname())
 
     # Instantiate a dummy authorizer for managing 'virtual' users
     authorizer = DummyAuthorizer()
@@ -13,14 +13,15 @@ def main():
     # Define a new user having full r/w permissions and a read-only
     # anonymous user
     authorizer.add_user("admin", "123456", ".", perm="elradfmwM")
-    # authorizer.add_anonymous(".", perm="elradfmwM")
+    authorizer.add_anonymous(".", perm="elradfmwM")
 
     # Instantiate FTP handler class
     handler = FTPHandler
     handler.authorizer = authorizer
 
     # Define a customized banner (string returned when client connects)
-    handler.banner = "pyftpdlib based ftpd ready."
+    # handler.banner = "pyftpdlib based ftpd ready."
+    handler.permit_foreign_addresses = True
 
     # Specify a masquerade address and the range of ports to use for
     # passive connections.  Decomment in case you're behind a NAT.
